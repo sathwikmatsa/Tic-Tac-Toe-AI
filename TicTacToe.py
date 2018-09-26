2b8d601294fcbeeacd3b1af0eca120a6e23bcf60
Sathwik Matsa <sathwikmatsa@gmail.com>
2018-09-26 19:02:27 +0530

    remove bugs in is_draw() method, updated UI
    UI enhancement - draws line to indicate winning sequence if the game's
    not draw

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,7 +6,7 @@ DISPLAYSURFACE = pygame.display.set_mode((380,380))
 BASICFONT = pygame.font.Font(None ,120)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
+RED = (255, 0, 0)
 
 ## board functions ##
 board = [[0,0,0],[0,0,0],[0,0,0]]
@@ -19,7 +19,7 @@ def get_winner():
 	# check rows
 	for i in range(3):
 		if abs(sum(board[i])) == 3:
-			return int(sum(board[i])/3)
+			return int(sum(board[i])/3), 'r', i
 
 	# check cols
 	for j in range(3):
@@ -27,23 +27,23 @@ def get_winner():
 		for i in range(3):
 			count+=board[i][j]
 		if abs(count) == 3:
-			return int(count/3)
+			return int(count/3), 'c', j
 
 	# check diagonals
 	count = 0
 	for i in range(3):
 		count+=board[i][i]
 	if abs(count) == 3:
-		return int(count/3)
+		return int(count/3), 'd', 0
 
 	count = 0
 	for i in range(3):
 		count+=board[i][2-i]
 	if abs(count) == 3:
-		return int(count/3)
+		return int(count/3), 'd', 1
 
 	# no winner
-	return 0
+	return [0]
 
 def is_draw():
 	count = 0
@@ -52,7 +52,7 @@ def is_draw():
 			if board[i][j] == 0:
 				count+=1
 
-	if count == 0:
+	if count == 0 and get_winner()[0] == 0:
 		return True
 	return False
 
@@ -87,7 +87,17 @@ def display_result():
 	if is_draw():
 		result = "DRAW!"
 	else:
-		result = "PLAYER " + ("1 " if get_winner() == 1 else "2 ") + "WINS!"
+		winner, collection_type, collection_index = get_winner()
+		result = "PLAYER " + ("1 " if winner == 1 else "2 ") + "WINS!"
+		if collection_type == 'r':
+			pygame.draw.line(DISPLAYSURFACE, RED, (20,20*(1+collection_index) + 50*(1+2*collection_index)), (360,20*(1+collection_index) + 50*(1+2*collection_index)), 25)
+		elif collection_type == 'c':
+			pygame.draw.line(DISPLAYSURFACE, RED, (20*(1+collection_index) + 50*(1+2*collection_index),20), (20*(1+collection_index) + 50*(1+2*collection_index),360), 25)
+		# diagonal case
+		elif collection_index == 0:
+			pygame.draw.line(DISPLAYSURFACE, RED, (20,20), (360,360), 25)
+		else:
+			pygame.draw.line(DISPLAYSURFACE, RED, (360,20), (20,360), 25)
 
 	RESULTFONT = pygame.font.Font(None ,40)
 	textSurf = RESULTFONT.render(result, True, WHITE)
@@ -95,7 +105,7 @@ def display_result():
 	textRect.center = DISPLAYSURFACE.get_rect().center
 	pygame.draw.rect(DISPLAYSURFACE, BLACK, textRect, 0)
 	DISPLAYSURFACE.blit(textSurf, textRect)
-	pygame.display.update(textRect)
+	pygame.display.flip()
 
 def new_game_requested():
 	DIALOGFONT = pygame.font.Font(None ,20)
@@ -114,15 +124,14 @@ def new_game_requested():
 
 
 def main():
-	done = False
 	new_board()
 	FPSCLOCK = pygame.time.Clock()
 	player = 1
 	render_board()
-	while not done and not is_draw() and get_winner() == 0:
+	while not is_draw() and get_winner()[0] == 0:
 		for event in pygame.event.get():
 			if event.type == QUIT:
-				done = True
+				return
 			elif event.type == MOUSEBUTTONDOWN:
 				x, y = event.pos
 				if update_board(x, y, player):
